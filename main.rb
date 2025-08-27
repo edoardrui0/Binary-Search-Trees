c095c40d432d97adc3c34231721c573eda91b424
Edoardo Ruiz <44981900+edoardrui0@users.noreply.github.com>
2025-08-27 13:27:50 -0400

    found mid of tree, set root

--- main.rb
+++ main.rb
@@ -1,7 +1,18 @@
 class Tree
-  attr_accessor :root, :tree
+  attr_accessor :tree
 
   def initialize(tree)
     @tree = tree
   end
+
+  def build_tree
+    sorted = tree.sort.uniq
+    mid = (sorted.length / 2).floor
+    root = sorted[mid]
+  end
 end
+
+arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+new = Tree.new(arr)
+
+p new.build_tree
