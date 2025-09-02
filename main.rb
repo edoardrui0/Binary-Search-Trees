e062febef172f25a125dea14bda6873f6bbc0d48
Edoardo Ruiz <44981900+edoardrui0@users.noreply.github.com>
2025-09-02 16:51:57 -0400

    set left & right of tree
    added pretty_print from TOP

--- main.rb
+++ main.rb
@@ -1,18 +1,32 @@
-class Tree
-  attr_accessor :tree
+require_relative 'node'
 
-  def initialize(tree)
-    @tree = tree
+class Tree
+  attr_accessor :root
+
+  def initialize(arr)
+    @root = build_tree(arr.sort.uniq)
   end
 
-  def build_tree
-    sorted = tree.sort.uniq
-    mid = (sorted.length / 2).floor
-    root = sorted[mid]
+  def build_tree(arr)
+    return nil if arr.empty?
+
+    mid = arr.length / 2
+    root = Node.new(arr[mid])
+
+    root.left = build_tree(arr[0...mid])
+    root.right = build_tree(arr[(mid + 1)..])
+
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
 
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-new = Tree.new(arr)
+tree = Tree.new(arr)
 
-p new.build_tree
+p tree.pretty_print
